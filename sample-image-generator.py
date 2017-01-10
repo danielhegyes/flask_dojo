979a1176cd76a5f5175a8d98533cbb65d99fa3b6
DAni <danielhegyes42@gmail.com>
2017-01-10 11:54:01 +0100

    test db added

--- sample-image-generator.py
+++ sample-image-generator.py
@@ -2,14 +2,14 @@ from PIL import Image
 from PIL import ImageFont
 from PIL import ImageDraw
 
-img = Image.new("RGB", (512, 512), "red")
+img = Image.new("RGB", (512, 512), "blue")
 draw = ImageDraw.Draw(img)
 # font = ImageFont.truetype(<font-file>, <font-size>)
 # font = ImageFont.truetype("sans-serif.ttf", 16)
 text_options = {
     'fill': (255, 255, 255)
 }
-text_content = "Sample Text"
+text_content = "send  nudes"
 text_size = draw.textsize(text_content)
 # draw.text((x, y),text_content,(r,g,b))
 draw.text((0, 0), text_content, **text_options)
