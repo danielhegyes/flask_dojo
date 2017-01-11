1cf087e4e59cfd303f82d2a9c6e645600366ddfb
If you're reading this, you can read! <danielhegyes42@gmail.com>
2017-01-11 09:19:38 +0100

    Add files via upload

---
+++ get_sql.py
@@ -0,0 +1,16 @@
+import psycopg2
+
+def runSql(query):
+    try:
+        # gitben database_config.txt-be elmenteni zet a stringet
+        connect_str = "dbname='dumbohill' user='dumbohill' host='localhost' password='sokszorkell'"
+        conn = psycopg2.connect(connect_str)
+        conn.autocommit = True
+        cursor = conn.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+    except Exception as e:
+        print("Uh oh, can't connect. Invalid dbname, user or password?")
+        print(e)
+
+
